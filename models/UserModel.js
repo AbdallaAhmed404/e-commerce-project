380e95701131cc5829f6810ec6f1fe7398e55b2b
AbdallaAhmed404 <abdallaahmedgym@gmail.com>
2025-07-07 23:50:28 +0300

    fix: update

--- models/UserModel.js
+++ models/UserModel.js
@@ -12,7 +12,7 @@ const userSchema = new mongoose.Schema(
     Name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    mobile: { type: String, required: true },
+    mobile: { type: String},
     resetCode: { type: String, select: false }
   },
   {
